5b24fdb60e18b3081e55c7b2fc41212257cee3fd
Bernard Maltais <bernard.maltais@ssc-spc.gc.ca>
2026-03-19 19:39:59 -0400

    Downgrade linux_VMs module version to v1.0.6

--- linux-vms.tf
+++ linux-vms.tf
@@ -1,5 +1,5 @@
 module "linux_VMs" {
-  source = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-linux_virtual_machineV2.git?ref=v1.0.9"
+  source = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-linux_virtual_machineV2.git?ref=v1.0.6"
   for_each = var.linux_vms_cluster.linux_VMs
 
   location= var.location
