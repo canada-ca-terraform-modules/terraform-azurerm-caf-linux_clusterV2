9116612e9bfee234262fdcfe1cca94750ee982e9
Bernard Maltais <bernard.maltais@ssc-spc.gc.ca>
2026-03-19 20:55:52 -0400

    Update terraform-azurerm-caf-linux_clusterV2 module ref from v1.0.2 to v1.1.0

--- ESLZ/SRV-Linux-cluster.tf
+++ ESLZ/SRV-Linux-cluster.tf
@@ -7,7 +7,7 @@ variable "linux_vms_clusterV2" {
 module "linux_VMs_clusterV2" {
 
   for_each          = var.linux_vms_clusterV2
-  source            = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-linux_clusterV2.git?ref=v1.0.2"
+  source            = "github.com/canada-ca-terraform-modules/terraform-azurerm-caf-linux_clusterV2.git?ref=v1.1.0"
   location          = var.location
   env               = var.env
   group             = var.group
